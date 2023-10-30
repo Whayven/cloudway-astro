9ffd05108df759b69400873b56694c3f5fa97c9e
Wayne <waynefosterjr@hotmail.com>
2023-10-30 00:00:53 -0400

    removed unused imports

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -52,11 +52,11 @@ const Header = ({ pathname }: HeaderProps) => {
             <text
               x="5"
               y="40"
-              font-family="Arial"
-              font-size="40"
+              fontFamily="Arial"
+              fontSize="40"
               fill="none"
               stroke="url(#nebulaGradient)"
-              stroke-width="2"
+              strokeWidth="2"
             >
               CW
             </text>
@@ -77,7 +77,7 @@ const Header = ({ pathname }: HeaderProps) => {
                 viewBox="0 0 16 16"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
                 />
               </svg>
