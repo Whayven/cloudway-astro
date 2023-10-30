bece88ea1ec9424ccbb343aa7bc128046a9bb77f
Wayne <waynefosterjr@hotmail.com>
2023-10-29 21:34:57 -0400

    home page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,75 @@
+const Footer = () => {
+  return (
+    <footer className="flex flex-wrap justify-center py-4 bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700">
+      <div className="max-w-[85rem] w-full mx-auto px-4 flex flex-col sm:flex-row sm:items-center sm:justify-between">
+        <div className="flex items-center justify-between mb-4 sm:mb-0">
+          <svg
+            className="astro-a"
+            width="80"
+            height="40"
+            viewBox="0 0 100 50"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <defs>
+              <linearGradient
+                id="nebulaGradientFooter"
+                x1="0%"
+                y1="0%"
+                x2="100%"
+                y2="100%"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop
+                  offset="0%"
+                  style={{ stopColor: "white", stopOpacity: 1 }}
+                />
+                <stop
+                  offset="50%"
+                  style={{ stopColor: "skyblue", stopOpacity: 1 }}
+                />
+                <stop
+                  offset="100%"
+                  style={{ stopColor: "purple", stopOpacity: 1 }}
+                />
+              </linearGradient>
+            </defs>
+
+            <text
+              x="5"
+              y="30"
+              font-family="Arial"
+              font-size="30"
+              fill="none"
+              stroke="url(#nebulaGradientFooter)"
+              stroke-width="2"
+            >
+              CW
+            </text>
+          </svg>
+        </div>
+
+        <div className="flex flex-col gap-2 sm:flex-row sm:gap-5">
+          <a
+            href="#"
+            className="text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white transition-colors duration-200"
+          >
+            Privacy Policy
+          </a>
+          <a
+            href="#"
+            className="text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white transition-colors duration-200"
+          >
+            Terms of Service
+          </a>
+          <span className="text-gray-600 dark:text-gray-500">
+            © 2023 CW. All Rights Reserved.
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+import preline from "preline/plugin.js";
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "node_modules/preline/dist/*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      backgroundColor: {
+        "custom-light": "#ffffff",
+        "custom-dark": "#13151a",
+        "card-dark": "#23262d",
+      },
+      textColor: {
+        "custom-light": "#ffffff",
+        "custom-dark": "#13151a",
+        "card-dark": "#23262d",
+      },
+      gradientColorStops: {
+        "accent-light": "rgb(255, 255, 255)",
+        accent: "rgb(135, 206, 235)",
+        "accent-dark": "rgb(128, 0, 128)",
+      },
+    },
+  },
+  plugins: [preline],
+};
