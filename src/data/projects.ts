33297b0cf29faab2ebc3e4f9435137c79fe055d9
Wayne <waynefosterjr@hotmail.com>
2023-10-29 23:39:54 -0400

    moved projects to data

---
+++ src/data/projects.ts
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    title: "Janet & Ray's",
+    description: "Check out this hidden gem in Tuscon, Arizona.",
+    imageUrl: "/janetandrays.png",
+    projectUrl: "https://janetandrays.com",
+  },
+  {
+    title: "Waytech",
+    description: "Enhance your business with Waytech.",
+    imageUrl: "/waytech.png",
+    projectUrl: "https://wayytech.com",
+  },
+  {
+    title: "Ascend Hiring",
+    description: "Hire the best talent with Ascend Hiring.",
+    imageUrl: "/ascendhiring.png",
+    projectUrl: "https://ascendhiring.com",
+  },
+];
