d78b8bf3fc2c4523877633aa75661b842e571ffc
Wayne <waynefosterjr@hotmail.com>
2023-10-30 21:02:03 -0400

    moved images to src and added project card

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,20 +1,24 @@
+import janetImage from "../assets/images/janetandrays.png";
+import waytechImage from "../assets/images/waytech.png";
+import ascendImage from "../assets/images/ascendhiring.png";
+
 export const projects = [
   {
     title: "Janet & Ray's",
     description: "Check out this hidden gem in Tuscon, Arizona.",
-    imageUrl: "/janetandrays.png",
+    imageUrl: janetImage,
     projectUrl: "https://janetandrays.com",
   },
   {
     title: "Waytech",
     description: "Enhance your business with Waytech.",
-    imageUrl: "/waytech.png",
+    imageUrl: waytechImage,
     projectUrl: "https://wayytech.com",
   },
   {
     title: "Ascend Hiring",
     description: "Hire the best talent with Ascend Hiring.",
-    imageUrl: "/ascendhiring.png",
+    imageUrl: ascendImage,
     projectUrl: "https://ascendhiring.com",
   },
 ];
